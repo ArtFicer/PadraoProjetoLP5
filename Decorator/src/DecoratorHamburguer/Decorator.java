add8085e48bc00e201cc5ab22073942bb3871ee7
Pedro Ventura <>
2018-06-05 00:18:36 -0300

    Mais exemplos decorator

--- Decorator/src/DecoratorHamburguer/Decorator.java
+++ Decorator/src/DecoratorHamburguer/Decorator.java
@@ -5,20 +5,48 @@ package DecoratorHamburguer;
 public class Decorator {
 
     public static void main(String[] args) {
-        Hamburguer meuHamburguer = new CheeseBacon();
-        System.out.println(meuHamburguer.getNome()+ " = "
-                + meuHamburguer.getPreco());
-        meuHamburguer = new Bacon(meuHamburguer);
-        System.out.println(meuHamburguer.getNome()+ " = "
-                + meuHamburguer.getPreco());
-        meuHamburguer = new Ovo(meuHamburguer);
-        System.out.println(meuHamburguer.getNome()+ " = "
-                + meuHamburguer.getPreco());
-        meuHamburguer = new Queijo(meuHamburguer);
-        System.out.println(meuHamburguer.getNome()+ " = "
-                + meuHamburguer.getPreco());
-        
-        
+        Hamburguer CBacon = new CheeseBacon();
+        System.out.println(CBacon.getNome() + " = "
+                + CBacon.getPreco());
+        CBacon = new Bacon(CBacon);
+        System.out.println(CBacon.getNome() + " = "
+                + CBacon.getPreco());
+        CBacon = new Ovo(CBacon);
+        System.out.println(CBacon.getNome() + " = "
+                + CBacon.getPreco());
+        CBacon = new Queijo(CBacon);
+        System.out.println(CBacon.getNome() + " = "
+                + CBacon.getPreco());
+
+        System.out.println("\n");
+
+        Hamburguer CBurguer = new CheeseBurguer();
+        System.out.println(CBurguer.getNome() + " = "
+                + CBurguer.getPreco());
+        CBurguer = new Bacon(CBurguer);
+        System.out.println(CBurguer.getNome() + " = "
+                + CBurguer.getPreco());
+        CBurguer = new Ovo(CBurguer);
+        System.out.println(CBurguer.getNome() + " = "
+                + CBurguer.getPreco());
+        CBurguer = new Queijo(CBurguer);
+        System.out.println(CBurguer.getNome() + " = "
+                + CBurguer.getPreco());
+
+        System.out.println("\n");
+
+        Hamburguer CEgg = new CheeseEgg();
+        System.out.println(CEgg.getNome() + " = "
+                + CEgg.getPreco());
+        CEgg = new Bacon(CEgg);
+        System.out.println(CEgg.getNome() + " = "
+                + CEgg.getPreco());
+        CEgg = new Ovo(CEgg);
+        System.out.println(CEgg.getNome() + " = "
+                + CEgg.getPreco());
+        CEgg = new Queijo(CEgg);
+        System.out.println(CEgg.getNome() + " = "
+                + CEgg.getPreco());
     }
 
 }
